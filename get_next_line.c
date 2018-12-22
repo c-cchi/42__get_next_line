e69cade542ddfc3ca4370975ecca70056ec1e20a
Chia-han CHI <cchi@e1r10p8.42.fr>
2018-12-22 19:58:15 +0100

    Code refactoring

--- get_next_line.c
+++ get_next_line.c
@@ -6,70 +6,50 @@
 /*   By: cchi <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/20 16:33:21 by cchi              #+#    #+#             */
-/*   Updated: 2018/12/22 19:21:41 by cchi             ###   ########.fr       */
+/*   Updated: 2018/12/22 19:56:59 by cchi             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#include <stdio.h>
 
-static char	*strtoreturn(char *str, char *s, unsigned int i, char **line)
+static char		*strtoreturn(char *str, char *s, char **line)
 {
-	unsigned int j;
-	char	*swap;
+	char			*swap;
 
-	j = 0;
-	j = s - (str + i);
-	if (str[1] == '\n')
-	{
-		*line = ft_strnew(0);
-		if (str[2])
-			swap = ft_strdup(&str[2]);
-		else
-			swap = ft_strnew(0);
-	}
+	*line = ft_strsub(str, 0, s - str);
+	if (*s != '\0')
+		swap = ft_strdup(s + 1);
 	else
-	{
-		*line = ft_strsub(str, i, j);
-		swap = ft_strdup(&str[i + j]);
-	}
+		swap = ft_strnew(0);
 	ft_strdel(&str);
 	return (swap);
 }
 
-int	get_next_line(const int fd, char **line)
+int				get_next_line(const int fd, char **line)
 {
 	char			buf[BUFF_SIZE + 1];
 	static char		*str = NULL;
-	unsigned int	i;
 	int				nbread;
 	char			*temp;
 	char			*s;
 
 	temp = NULL;
-	i = 0;
-	if (fd < 0 || !BUFF_SIZE || read(fd, 0, 0) == -1 || BUFF_SIZE < 0)
+	if (fd < 0 || BUFF_SIZE <= 0 || read(fd, 0, 0) == -1)
 		return (-1);
 	if (!str)
 		str = ft_strnew(0);
-	if (str[i] == '\n')
-		i++;
-	while ((nbread = read(fd, buf, BUFF_SIZE)) > 0 && !(ft_strchr((str + i), '\n')))
+	while (!(s = ft_strchr(str, '\n'))
+		&& (nbread = read(fd, buf, BUFF_SIZE)) > 0)
 	{
 		buf[nbread] = '\0';
-		temp = ft_strjoin((str), buf);
+		temp = ft_strjoin(str, buf);
 		ft_strdel(&str);
 		str = temp;
-		if (ft_strchr((str + i), '\n'))
-			break ;
 	}
-	if ((s = ft_strchr((str + i), '\n')) 
-			|| ((s = (str + i + ft_strlen((str + i)))) && nbread == 0 && ft_strlen(str + i)))
+	if (s || ((s = ft_strchr(str, '\0')) && nbread == 0 && str[0] != '\0'))
 	{
-		str = strtoreturn(str, s, i, line);
+		str = strtoreturn(str, s, line);
 		return (1);
 	}
-	if (nbread == 0 && ft_strlen(str) == 0)
-		return (0);
 	return (0);
 }
